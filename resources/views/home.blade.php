f457849614c2fee63cd459a7b843ead252d3eafb
MurtaghLuke <l.murtagh2m@gmail.com>
2025-04-18 17:46:42 +0100

    Setup Laravel mix/tailwind and fixed errors. created welcoming page and updated using tailwind

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Movie Suggestor</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+</head>
+<body class="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center">
+    <div class="text-center px-4">
+        <h1 class="text-5xl font-bold mb-4">Movie Suggestor</h1>
+        <p class="text-xl mb-6">Find the perfect movie for you!.</p>
+        <a href="#" class="bg-indigo-600 text-white font-bold py-2 px-6 rounded-full">
+            Get Started
+        </a>
+    </div>
+</body>
+</html>
