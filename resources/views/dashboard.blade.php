248fffd05c5da31fdad006e789ede3210aa4f19b
MurtaghLuke <l.murtagh2m@gmail.com>
2025-05-04 15:49:45 +0100

    Make grid headings bold. Added padding between text in banner.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,9 @@
 <section class="bg-gradient-to-r from-red-600 to-blue-600 text-white py-20 text-center">
     {{-- max width 4x1. horizontal margin is auto, and padding of 4px --}}
     <div class="max-w-4xl mx-auto px-4">
-        <h1 class="text-5xl font-extrabold">Welcome to our Movie Suggestor</h1>
+        <h1 class="text-5xl font-extrabold mb-4">Welcome to our Movie Suggestor</h1>
         {{-- text extra large. margin bottom of 4 --}}
-        <p class="text-xl mb-4">Find the next film for you.</p>
+        <p class="text-xl mb-8">Find the next film for you.</p>
         {{-- movie suggestion button. bg is white. text indigo. rounded and changes color when hovered on.            transition duration-300 ease-in-out  adds animation to button when hovered on--}}    
         <a href="{{ route('suggest.form') }}"
            class="bg-white text-indigo-700 font-semibold px-6 py-3 rounded-full shadow hover:bg-gray-100 transition duration-300 ease-in-out">
@@ -31,17 +31,17 @@
     <div class="max-w-6xl mx-auto px-4 grid gap-10 grid-cols-1 md:grid-cols-3 text-center">
         <div>
             <div class="text-4xl mb-2">🔍</div>
-            <h3 class="text-xl mb-2">Smart Suggestions</h3>
+            <h3 class="text-xl font-bold mb-2">Smart Suggestions</h3>
             <p class="text-gray-600 dark:text-gray-300">Pick a genre and let the system suggest something for you.</p>
         </div>
         <div>
             <div class="text-4xl mb-2"> 😮 </div>
-            <h3 class="text-xl mb-2">Surprise Me! Feature</h3>
+            <h3 class="text-xl font-bold mb-2">Surprise Me! Feature</h3>
             <p class="text-gray-600 dark:text-gray-300">Looking for something different? Use our random movie generator.</p>
         </div>
         <div>
             <div class="text-4xl mb-2">⚙️</div>
-            <h3 class="text-xl mb-2">Admin Features</h3>
+            <h3 class="text-xl font-bold mb-2">Admin Features</h3>
             <p class="text-gray-600 dark:text-gray-300">Add or delete movies in the movies page (admin only).</p>
         </div>
     </div>
