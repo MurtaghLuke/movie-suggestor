f9f201528dc5b60c2a093ea3c0f355ad471af35a
MurtaghLuke <l.murtagh2m@gmail.com>
2025-05-07 19:30:33 +0100

    deployment

--- startup.sh
+++ startup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-cd /home/site/wwwroot_old
+cd /home/site/wwwroot
 
-# Set permissions so Laravel can write to these folders
 chmod -R 755 storage bootstrap/cache
 
-# Laravel caching steps
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
