b13c6a94508bad879007304a83d45bd33f08cb71
MurtaghLuke <l.murtagh2m@gmail.com>
2025-05-07 21:49:25 +0100

    final deploy? removed dashboard from behind auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,9 @@
     <div class="text-center px-4">
         <h1 class="text-5xl font-bold mb-4">Movie Suggestor</h1>
         <p class="text-xl mb-6">Find the perfect movie for you!.</p>
-        <a href="#" class="bg-indigo-600 text-white font-bold py-2 px-6 rounded-full">
+        <a href="{{ route('dashboard') }}" class="bg-indigo-600 text-white font-bold py-2 px-6 rounded-full">
             Get Started
-        </a>
+        </a>      
     </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::resource('movies', \App\Http\Controllers\MovieController::class);
 
 
 //update method for scroll wheel on homepage
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
